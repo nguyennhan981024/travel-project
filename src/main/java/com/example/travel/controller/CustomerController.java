6fb5e8c5bb7364f1071aba300c0c052c36f0be9c
nguyennhan981024 <nguyennhan981024@gmail.com>
2021-05-21 00:36:14 +0700

    travel app

---
+++ src/main/java/com/example/travel/controller/CustomerController.java
@@ -0,0 +1,9 @@
+package com.example.travel.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(path = "/admin/customers")
+public class CustomerController {
+}
